56f27ed1bbd24bad6c5f35786a0fc75e2ff0231f
Walid <walid.msallem@gomycode.co>
2020-02-09 02:34:56 +0100

    second commit

--- src/shared/components/sideBar/sideBar.jsx
+++ src/shared/components/sideBar/sideBar.jsx
@@ -1,20 +1,18 @@
-import React  , {useState , useEffect} from 'react';
-import axios from 'axios'
+import React  , {useEffect,useState} from 'react';
+import data from './fakeData.json'
 import {Tree} from 'primereact/tree';
 import './sideBar.scss'
+
+
 const SideBar = () => {
-    // const [nodes, setNodes] = useState(20)
-    // const [state, setstate] = useState(0)
-    useEffect(() => {
-        console.log('jj')
-     
-    }, []);
+
     return (
         <div className ='side-bar'>
              <div className ='side-bar__upload-container'>
                  Upload
              </div>
-             
+
+             <Tree value={data} />
         </div>
     );
 }
